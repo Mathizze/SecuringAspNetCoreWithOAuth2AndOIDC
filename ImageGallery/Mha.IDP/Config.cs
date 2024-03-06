9d345d0d1d82d8d0e2a187d8c05da4b75efb41c7
Mathijs Hardeman <mathijs.hardeman@wolterskluwer.com>
2024-03-06 19:21:16 +0100

    module 3: Understanding Authentication with OpenID Connect

---
+++ ImageGallery/Mha.IDP/Config.cs
@@ -0,0 +1,22 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Mha.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+                { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+                { };
+    }
+}
---
+++ ImageGallery/Mha.IDP/Pages/TestUsers.cs
@@ -0,0 +1,91 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using IdentityModel;
+using System.Security.Claims;
+using System.Text.Json;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Test;
+
+namespace Mha.IDP;
+
+public static class TestUsers
+{
+    public static List<TestUser> Users
+    {
+        get
+        {
+            var address = new
+            {
+                street_address = "One Hacker Way",
+                locality = "Heidelberg",
+                postal_code = "69118",
+                country = "Germany"
+            };
+                
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                    Username = "David",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        //new Claim("role", "FreeUser"),
+                        new Claim(JwtClaimTypes.GivenName, "David"),
+                        new Claim(JwtClaimTypes.FamilyName, "Flagg"),
+                        //new Claim("country", "nl")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                    Username = "Emma",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        //new Claim("role", "PayingUser"),
+                        new Claim(JwtClaimTypes.GivenName, "Emma"),
+                        new Claim(JwtClaimTypes.FamilyName, "Flagg"),
+                        //new Claim("country", "be")
+                    }
+                }
+                /*new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "alice",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                        new Claim(JwtClaimTypes.GivenName, "Alice"),
+                        new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                        new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                        new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "bob",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                        new Claim(JwtClaimTypes.GivenName, "Bob"),
+                        new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                        new Claim(JwtClaimTypes.Email, "BobSmith@email.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                        new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                }*/
+            };
+        }
+    }
+}
